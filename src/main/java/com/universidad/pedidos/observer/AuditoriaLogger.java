ef5194e7739584e92775f7f00b4040b2cecef9e6
JahirBautistaQ <02210132008@mail.udes.edu.co>
2026-04-13 13:59:38 -0500

    Paso 1 — Observer: Eventos de Pedido con Spring

---
+++ src/main/java/com/universidad/pedidos/observer/AuditoriaLogger.java
@@ -0,0 +1,20 @@
+package com.universidad.pedidos.observer;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuditoriaLogger {
+
+    @EventListener
+    public void onConfirmado(PedidoConfirmadoEvent e) {
+        System.out.println("[AUDITORIA] Pedido "
+                + e.getPedido().getId() + " -> CONFIRMADO");
+    }
+
+    @EventListener
+    public void onCancelado(PedidoCanceladoEvent e) {
+        System.out.println("[AUDITORIA] Pedido "
+                + e.getPedido().getId() + " -> CANCELADO");
+    }
+}
